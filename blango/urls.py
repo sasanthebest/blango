943d7062b93ffd19b58234bf9b10a03172f1fc83
Sasan mahmoodi <mahmoodi.sasan99@gmail.com>
2023-04-19 14:59:54 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'',include('blog.urls'))
+    path(r'',include('blog.urls')),
+    path("api/v1/", include("blog.api_urls")),
 ]
